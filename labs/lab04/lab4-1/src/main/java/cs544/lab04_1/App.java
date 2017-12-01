eb73ef445604d9052aabc6efcb696098b5527ff5
David Otiego <davidotiego@yahoo.co.uk>
2017-11-30 23:25:42 -0600

    lab04
    JPQL samples as per problems

--- labs/lab04/lab4-1/src/main/java/cs544/lab04_1/App.java
+++ labs/lab04/lab4-1/src/main/java/cs544/lab04_1/App.java
@@ -43,16 +43,26 @@ public class App {
 			tx.begin();
 
 			logger.trace("List all the flights:");
-			List<Flight> flights = em.createQuery("from Flight").getResultList();
-			logger.trace(String.format("%-9s%-31s%-31s", "Flight:", "Departs:",
-					"Arrives:"));
+			// All flights leaving the USA with a capacity > 500
+			// List<Flight> flights = em.createQuery("from Flight f"
+			// + " WHERE f.airplane.capacity > 500").getResultList();
+
+			// All airlines that use A380 (model) airplanes
+			// List<Flight> flights = em.createQuery("from Flight f"
+			// + " WHERE f.airplane.model = 'A380'").getResultList();
+
+			// All fights using 747 planes that don’t belong to ‘Star Alliance’
+			// List<Flight> flights = em.createQuery("from Flight f"
+			// + " WHERE f.airplane.model = '747'").getResultList();
+
+			// All flights leaving before 12pm on 08/07/2009
+			List<Flight> flights = em.createQuery("from Flight f"
+					+ " WHERE f.departureDate = '2009-08-07' AND f.departureTime < '12:00'").getResultList();
+			logger.trace(String.format("%-9s%-31s%-31s", "Flight:", "Departs:", "Arrives:"));
 			for (Flight flight : flights) {
-				logger.trace(String.format(
-						"%-7s  %-12s %7s %8s  %-12s %7s %8s",
-						flight.getFlightnr(), flight.getOrigin().getCity(),
-						flight.getDepartureDate(), flight.getDepartureTime(),
-						flight.getDestination().getCity(),
-						flight.getArrivalDate(), flight.getArrivalTime()));
+				logger.trace(String.format("%-7s  %-12s %7s %8s  %-12s %7s %8s", flight.getFlightnr(),
+						flight.getOrigin().getCity(), flight.getDepartureDate(), flight.getDepartureTime(),
+						flight.getDestination().getCity(), flight.getArrivalDate(), flight.getArrivalTime()));
 			}
 			tx.commit();
 		} catch (PersistenceException e) {
@@ -81,17 +91,17 @@ public class App {
 
 			Airline airline1 = new Airline("Delta");
 
-			Flight flight1 = new Flight("DE 36", "08/06/2009", "7:10 pm",
-					"06/25/2015", "9:00 am", airline1, mia, mco, airplane1);
+			Flight flight1 = new Flight("DE 36", "08/06/2009", "7:10 pm", "06/25/2015", "9:00 am", airline1, mia, mco,
+					airplane1);
 
 			em = emf.createEntityManager();
 			tx = em.getTransaction();
 			tx.begin();
-			
+
 			em.persist(flight1);
 
 			tx.commit();
-			
+
 		} catch (PersistenceException e) {
 			if (tx != null) {
 				logger.error("Rolling back", e);
